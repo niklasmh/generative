80d581d7938b3c1939aec2c4764efe0fb86afb0e
Niklas Molnes Hole <nikasmh@hotmail.com>
2021-05-30 15:42:40 +0200

    Justify solar system by image width and make it more detailed

--- src/projects/SolarSystem.js
+++ src/projects/SolarSystem.js
@@ -14,13 +14,15 @@ function draw() {
   proj.rotateX(45);
   proj.translate(0, 1, 5);
 
+  const details = Math.sqrt(this.width / 500);
+
   background(0);
   sort(
     union([
-      sun(1, [255, 200, 120], 190, 5),
-      planet(2, -280, 0.3, [0, 255, 0], 50, 30),
-      planet(3, -45, 0.3, [0, 255, 255], 50, 30),
-      planet(3.7, -140, 0.5, [100, 100, 100], 50, 30),
+      sun(1, [255, 200, 120], 190 * details, 5 / details / 4),
+      planet(2, -280, 0.3, [0, 255, 0], 50 * details, 30 / details / 4),
+      planet(3, -45, 0.3, [0, 255, 255], 50 * details, 30 / details / 4),
+      planet(3.7, -140, 0.5, [100, 100, 100], 50 * details, 30 / details / 4),
     ])
   ).forEach(([x, y, _, color, pointSize]) => {
     strokeWeight(pointSize);
@@ -51,7 +53,7 @@ function sun(radius, color, details, dotSize) {
 function planet(distance, angle, radius, color, details = 50, dotSize = 10) {
   stroke(255);
   const circumference = Math.PI * distance * 2;
-  const stepSize = 0.2;
+  const stepSize = 0.08;
   const steps = Math.floor(circumference / stepSize);
 
   const angleRad = d2r(angle);
@@ -60,10 +62,11 @@ function planet(distance, angle, radius, color, details = 50, dotSize = 10) {
 
   return union([
     mapTo3D(dottedCircle(0, 0, distance, steps)).map(([x, y, size]) => {
-      return [x, y, size, [255, 255, 255], size * 10];
+      const color = 100;
+      return [x, y, size, [color, color, color], size * 1];
     }),
     mapTo3D(dottedSphere(x0, y0, 0, radius, details), true).map(
-      ([x, y, size, ox, oy, oz]) => {
+      ([x, y, size, ox, oy]) => {
         const dx = ox - x0;
         const dy = oy - y0;
         const dotProduct = dx * x0 + dy * y0;
